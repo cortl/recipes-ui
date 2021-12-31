90fd17388affd107886e5000d4c6c58a34fbd70f
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2021-12-31 14:30:59 -0600

    unused import

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,4 @@
-import { ApolloServer, gql } from 'apollo-server-micro'
+import { ApolloServer } from 'apollo-server-micro'
 import type { NextApiRequest, NextApiResponse, PageConfig } from 'next'
 
 import { typeDefs } from '../../graphql/types'
