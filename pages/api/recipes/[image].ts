0cb18659690805a87a408684f7707c1e1fbc2b67
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2022-02-03 20:45:00 -0600

    some way of serving images

---
+++ pages/api/recipes/[image].ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { image } = req.query;
+
+  const path = `./node_modules/@cortl/recipes/images/${image}`;
+  const fileExists = fs.existsSync(path);
+
+  if (fileExists) {
+    const foundImage = fs.readFileSync(path);
+
+    console.log(foundImage);
+    res.status(200).setHeader("Content-Type", "image/jpg").write(foundImage);
+    res.end();
+  } else {
+    res.status(404).send("Not Found");
+  }
+};
+
+export default handler;
