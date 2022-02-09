09f7eacdb5a557ea395e1ef4c115434c85634b9f
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2022-02-09 09:36:33 -0600

    dont show an image if there isnt one to be displayed

--- src/client/domain/recipe-card.tsx
+++ src/client/domain/recipe-card.tsx
@@ -23,35 +23,44 @@ interface IRecipeCard {
   slug: string;
   image: string;
   tags: string[];
-  time: Recipe['time']
+  time: Recipe["time"];
 }
 
-const RecipeCard: React.FC<IRecipeCard> = ({ image, title, slug, tags, time }) => (
-  <Card>
-    <LinkBox>
-      <Image src={image} />
-      <Link href={`/${slug}`} passHref>
-        <LinkOverlay />
-      </Link>
-    </LinkBox>
-    <Box p="6">
-      <ProteinTags slug={slug} tags={tags} />
-      <MealTypeTags slug={slug} tags={tags} />
-      <MethodTags slug={slug} tags={tags} />
-      <HolidayTags slug={slug} tags={tags} />
-      
-      <Stack mt={1}>
-        <TimeTag time={time}/>
-      </Stack>
-      <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
-        <Link href={`/${slug}`}>
-          <CLink>
-            <Text isTruncated>{title}</Text>
-          </CLink>
-        </Link>
-      </Box>
-    </Box>
-  </Card>
-);
+const RecipeCard: React.FC<IRecipeCard> = ({
+  image,
+  title,
+  slug,
+  tags,
+  time,
+}) => {
+  return (
+    <Card>
+      {image && (
+        <LinkBox>
+          <Image src={image} />
+          <Link href={`/${slug}`} passHref>
+            <LinkOverlay />
+          </Link>
+        </LinkBox>
+      )}
+      <Box p="6">
+        <ProteinTags slug={slug} tags={tags} />
+        <MealTypeTags slug={slug} tags={tags} />
+        <MethodTags slug={slug} tags={tags} />
+        <HolidayTags slug={slug} tags={tags} />
 
+        <Stack mt={1}>
+          <TimeTag time={time} />
+        </Stack>
+        <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
+          <Link href={`/${slug}`}>
+            <CLink>
+              <Text isTruncated>{title}</Text>
+            </CLink>
+          </Link>
+        </Box>
+      </Box>
+    </Card>
+  );
+};
 export { RecipeCard };
--- src/server/graphql/resolvers/recipe.ts
+++ src/server/graphql/resolvers/recipe.ts
@@ -1,82 +1,107 @@
-import Recipes from '@cortl/recipes';
-import { UserInputError } from 'apollo-server-micro'
-import Fuse from "fuse.js";;
+import Recipes from "@cortl/recipes";
+import { UserInputError } from "apollo-server-micro";
+import Fuse from "fuse.js";
 
-import { ArrayFilter, BooleanFilter, NumberFilter, RecipeInput, RecipesWhereInput } from '../../../../types/resolvers';
-import { getKeyValue, filterBoolean, filterArray, filterNumber } from '../../../utils/filters';
-import { sortByField } from '../../../utils/sort';
+import {
+  ArrayFilter,
+  BooleanFilter,
+  NumberFilter,
+  RecipeInput,
+  RecipesWhereInput,
+} from "../../../../types/resolvers";
+import {
+  getKeyValue,
+  filterBoolean,
+  filterArray,
+  filterNumber,
+} from "../../../utils/filters";
+import { sortByField } from "../../../utils/sort";
 
 const archivedResolver = ({ archived }: Recipe) => {
-    return Boolean(archived);
-}
+  return Boolean(archived);
+};
 
 const recipesResolver = (_root: undefined, args: RecipesWhereInput) => {
-    if (!args) {
-        return Recipes.asArray;
-    }
+  if (!args) {
+    return Recipes.asArray;
+  }
 
-    let results = Recipes.asArray;
+  let results = Recipes.asArray;
 
-    if (args.where) {
-        results = results.filter(recipe => {
-            const result = Object.entries(args.where).reduce((keep, [filterKey, filterValue]) => {
-                if (!keep) {
-                    return keep;
-                }
+  if (args.where) {
+    results = results.filter((recipe) => {
+      const result = Object.entries(args.where).reduce(
+        (keep, [filterKey, filterValue]) => {
+          if (!keep) {
+            return keep;
+          }
 
-                if (!filterValue) {
-                    return keep;
-                }
+          if (!filterValue) {
+            return keep;
+          }
 
-                const recipeField = getKeyValue<keyof Recipe, Recipe>(filterKey, recipe)
-                let recipeMatchesCriteria = true;
+          const recipeField = getKeyValue<keyof Recipe, Recipe>(
+            filterKey,
+            recipe
+          );
+          let recipeMatchesCriteria = true;
 
-                if (typeof recipeField === 'boolean' || recipeField == null) {
-                    recipeMatchesCriteria = recipeMatchesCriteria && filterBoolean(recipeField, filterValue as BooleanFilter)
-                }
+          if (typeof recipeField === "boolean" || recipeField == null) {
+            recipeMatchesCriteria =
+              recipeMatchesCriteria &&
+              filterBoolean(recipeField, filterValue as BooleanFilter);
+          }
 
-                if (recipeField instanceof Array) {
-                    recipeMatchesCriteria = recipeMatchesCriteria && filterArray(recipeField, filterValue as ArrayFilter)
-                }
+          if (recipeField instanceof Array) {
+            recipeMatchesCriteria =
+              recipeMatchesCriteria &&
+              filterArray(recipeField, filterValue as ArrayFilter);
+          }
 
-                if (typeof recipeField === 'number') {
-                    recipeMatchesCriteria = recipeMatchesCriteria && filterNumber(recipeField, filterValue as NumberFilter)
-                }
+          if (typeof recipeField === "number") {
+            recipeMatchesCriteria =
+              recipeMatchesCriteria &&
+              filterNumber(recipeField, filterValue as NumberFilter);
+          }
 
-                return recipeMatchesCriteria;
-            }, true);
+          return recipeMatchesCriteria;
+        },
+        true
+      );
 
-            return result;
-        })
-    }
+      return result;
+    });
+  }
 
-    if (args.where.title?.like) {
-        const fuse = new Fuse(results, {
-            keys: ['title']
-        });
+  if (args.where.title?.like) {
+    const fuse = new Fuse(results, {
+      keys: ["title"],
+    });
 
-        results = fuse.search(args.where.title?.like).map(({ item }) => item);
-    } else if (args.sort) {
-        results.sort(sortByField(args.sort.field, args.sort.direction))
-    }
+    results = fuse.search(args.where.title?.like).map(({ item }) => item);
+  } else if (args.sort) {
+    results.sort(sortByField(args.sort.field, args.sort.direction));
+  }
 
-    const {offset, limit} = args;
+  const { offset, limit } = args;
 
-    return results.slice(offset, offset + limit);
-}
+  return results.slice(offset, offset + limit);
+};
 
 const recipeResolver = (_root: undefined, args: RecipeInput) => {
-    const recipe = Recipes.asArray.find((recipe) => recipe.slug === args.slug);
+  const recipe = Recipes.asArray.find((recipe) => recipe.slug === args.slug);
 
-    if (!recipe) {
-        throw new UserInputError(`${args} not found.`)
-    }
+  if (!recipe) {
+    throw new UserInputError(`${args} not found.`);
+  }
 
-    return recipe;
-}
+  return recipe;
+};
 
-const imageResolver = ({image}: Recipe) => {
-    return `http://localhost:3000/api/recipes/images/${image}`;
-}
+const imageResolver = ({ image }: Recipe) => {
+  if (image) return `http://localhost:3000/api/recipes/images/${image}`;
 
-export { archivedResolver, recipeResolver, imageResolver, recipesResolver }
+  return null;
+};
+
+export { archivedResolver, recipeResolver, imageResolver, recipesResolver };
