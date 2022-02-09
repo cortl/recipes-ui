73492e56de89f792c12f63ffa9f11575e280c79c
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2022-02-09 09:39:09 -0600

    handling when there are no results

--- src/client/domain/results-list.tsx
+++ src/client/domain/results-list.tsx
@@ -1,4 +1,5 @@
 import Masonry from "react-masonry-css";
+import { Center, Text } from "@chakra-ui/react";
 
 import { RecipeCard } from "./recipe-card";
 
@@ -13,24 +14,32 @@ const responsiveBreakpoints = {
 };
 
 const ResultsList: React.FC<IResultsList> = ({ recipes }) => {
-  return (
-    <Masonry
-      breakpointCols={responsiveBreakpoints}
-      className="masonry"
-      columnClassName="masonry-column"
-    >
-      {recipes.map(({ title, slug, image, tags, time }: Recipe, i) => (
-        <RecipeCard
-          key={`${i}-${slug}`}
-          slug={slug}
-          title={title}
-          image={image}
-          tags={tags}
-          time={time}
-        />
-      ))}
-    </Masonry>
-  );
+  if (recipes && recipes.length) {
+    return (
+      <Masonry
+        breakpointCols={responsiveBreakpoints}
+        className="masonry"
+        columnClassName="masonry-column"
+      >
+        {recipes.map(({ title, slug, image, tags, time }: Recipe, i) => (
+          <RecipeCard
+            key={`${i}-${slug}`}
+            slug={slug}
+            title={title}
+            image={image}
+            tags={tags}
+            time={time}
+          />
+        ))}
+      </Masonry>
+    );
+  } else {
+    return (
+      <Center pt='5'>
+        <Text>{"Looks like there isn't anything here... 😿"}</Text>
+      </Center>
+    );
+  }
 };
 
 export { ResultsList };
