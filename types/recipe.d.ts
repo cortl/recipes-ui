fb8b607196506af62beabadb52313fcb870d9d43
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2022-02-11 09:22:25 -0600

    removing old eslint config, adding typescript config to check for imports and whatever

--- types/recipe.d.ts
+++ types/recipe.d.ts
@@ -1,46 +1,46 @@
 type Ingredient = {
-    category: string;
-    items: string[]
-}
+  category: string;
+  items: string[];
+};
 
 type TimeUnit = {
-    measurement: number;
-    label: string;
-}
+  measurement: number;
+  label: string;
+};
 
 type Time = {
-    label: string;
-    units: TimeUnit[];
-}
+  label: string;
+  units: TimeUnit[];
+};
 
 type Recipe = {
-    title: string;
-    servings: number;
-    rating: number;
-    slug: string;
-    source: string;
-    createdDate: string;
-    instructions: string[];
-    notes: string[];
-    archived?: boolean;
-    ingredients: Ingredient[];
-    tags: string[];
-    time: Time[];
-    image: string;
+  title: string;
+  servings: number;
+  rating: number;
+  slug: string;
+  source: string;
+  createdDate: string;
+  instructions: string[];
+  notes: string[];
+  archived?: boolean;
+  ingredients: Ingredient[];
+  tags: string[];
+  time: Time[];
+  image: string;
 
-    [key: string]: unknown
-}
+  [key: string]: unknown;
+};
 
 interface AsMap {
-    [key: string]: Recipe
+  [key: string]: Recipe;
 }
 
-declare module '@cortl/recipes' {
-    declare const recipes: Recipe[]
-    declare const asMap: AsMap;
+declare module "@cortl/recipes" {
+  declare const recipes: Recipe[];
+  declare const asMap: AsMap;
 
-    export default {
-      asArray: recipes,
-      asMap: asMap,
-    };
+  export default {
+    asArray: recipes,
+    asMap: asMap,
+  };
 }
