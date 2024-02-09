b9057efed98f93012091b888f9a1356441b5c448
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2024-02-09 11:33:17 -0600

    doing some metric collection for a bit between firestore (lazy) and storage

--- src/server/repository/recipes.ts
+++ src/server/repository/recipes.ts
@@ -1,15 +1,16 @@
+/* eslint-disable no-console */
 import { Storage } from "@google-cloud/storage";
+import { Firestore } from "@google-cloud/firestore";
 
 import type { Recipe } from "../../types/recipe";
+import { timeFunction } from "../utils/timing";
 
 const RECIPE_BUCKET = "cortl-recipes-json";
 
 const storage = new Storage();
+const firestore = new Firestore();
 
-let cachedRecipes: Recipe[] | null = null;
-let lastFetchTime: Date | null = null;
-
-const getRecipe = async (slug: string): Promise<Recipe | null> => {
+const getRecipeFromStorage = async (slug: string): Promise<Recipe | null> => {
   const file = storage.bucket(RECIPE_BUCKET).file(`${slug}.json`);
 
   try {
@@ -20,31 +21,49 @@ const getRecipe = async (slug: string): Promise<Recipe | null> => {
 
     return recipe;
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error("Error fetching recipe:", error);
+    console.error("Error fetching recipe from bucket:", error);
 
-    return null; // Or handle the error as you prefer
+    return null;
   }
 };
 
-const getRecipes = async (): Promise<Recipe[]> => {
-  const oneHourAgo = new Date();
+const getRecipeFromFirestore = async (slug: string): Promise<Recipe | null> => {
+  const doc = firestore.collection("recipes").doc(slug);
 
-  oneHourAgo.setHours(oneHourAgo.getHours() - 1);
+  try {
+    const snapshot = await doc.get();
 
-  if (cachedRecipes && lastFetchTime && lastFetchTime > oneHourAgo) {
-    // eslint-disable-next-line no-console
-    console.log("Returning cached recipes");
+    if (!snapshot.exists) {
+      console.log(`Recipe ${slug} not found in Firestore`);
 
-    return cachedRecipes;
+      return null;
+    }
+
+    const recipe = snapshot.data() as Recipe;
+
+    return recipe;
+  } catch (error) {
+    console.error("Error fetching recipe from Firestore:", error);
+
+    return null;
   }
+};
 
+const getRecipe = async (slug: string): Promise<Recipe | null> => {
+  const [recipe] = await Promise.all([
+    timeFunction("(Recipe) Firestore", async () =>
+      getRecipeFromFirestore(slug),
+    ),
+    timeFunction("(Recipe) Storage", async () => getRecipeFromStorage(slug)),
+  ]);
+
+  return recipe;
+};
+
+const getRecipesFromStorage = async (): Promise<Recipe[]> => {
   const bucket = storage.bucket(RECIPE_BUCKET);
 
   try {
-    // eslint-disable-next-line no-console
-    console.log("Fetching new recipes");
-
     const [files] = await bucket.getFiles();
 
     const jsonFiles = files.filter((file) => file.name.endsWith(".json"));
@@ -57,20 +76,40 @@ const getRecipes = async (): Promise<Recipe[]> => {
       }),
     );
 
-    cachedRecipes = recipes;
-    lastFetchTime = new Date();
-
     return recipes;
-  } catch (error: unknown) {
-    // eslint-disable-next-line no-console
-    console.error("Error fetching recipes:", error);
+  } catch (error) {
+    console.error("Error fetching recipes from bucket:", error);
 
-    if (error instanceof Error) {
-      throw error;
-    }
-
-    throw new Error("Unable to fetch all recipes");
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw error;
   }
 };
 
+const getRecipesFromFirestore = async (): Promise<Recipe[]> => {
+  const collection = firestore.collection("recipes");
+
+  try {
+    const snapshot = await collection.get();
+
+    const recipes = snapshot.docs.map((doc) => doc.data() as Recipe);
+
+    return recipes;
+  } catch (error) {
+    console.error("Error fetching recipes from Firestore:", error);
+
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw error;
+  }
+};
+
+const getRecipes = async (): Promise<Recipe[]> => {
+  const [recipes] = await Promise.all([
+    timeFunction("(Recipes) Firestore", async () => getRecipesFromFirestore()),
+    timeFunction("(Recipes) Storage", async () => getRecipesFromStorage()),
+  ]);
+
+  return recipes;
+};
+
 export { getRecipe, getRecipes };
+/* eslint-enable no-console */
