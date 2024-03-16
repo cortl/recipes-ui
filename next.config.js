d3a5b7916579aaad3df1ad28c55ffa9bcfce0ac7
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2024-03-16 11:24:29 -0500

    update next config for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "recipes.cortlan.dev", "storage.googleapis.com"],
+    // remotePatterns: ["localhost", "recipes.cortlan.dev", "storage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000'
+      },
+      {
+        protocol: 'https',
+        hostname: 'recipes.cortlan.dev'
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/cortl-recipe-images/**'
+      }
+    ]
   },
 };
